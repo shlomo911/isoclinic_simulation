b94e55b91af50e535e0487234cff9bd4ee7b1691
Shlomo Danziger <103130982+shlomo911@users.noreply.github.com>
2023-09-18 16:55:55 +0300

    Update isoclinic_simulation_code.m

--- isoclinic_simulation_code.m
+++ isoclinic_simulation_code.m
@@ -1,19 +1,24 @@
 %% PHOTOELASTICITY EXPERIMENT: isoclinic simulation
+
+% This MATLAB file "isoclinic_simulation_code.m" contains code for simulating 
+% isoclinic patterns and animating isoclinic changes on a photoelastic disc, 
+% according to two theoretical models of physical stress maps - the Ovals model 
+% and the Wulff net model, as explained in the "photoelasticity_report.pdf" file 
+% (sections 1,2,4) and its appendix presentation file "photoelasticity report - 
+% video appendix.ppsx".
+% The "Photoelasticity presentation.ppsx" contains further demonstrations and 
+% animations of the isoclinic simlulation process.
+%%
+
+% Code, paper and presentations were created as part of a photoelsticity experiment, 
+% carried out in LAB B course of our undergarduate physics studies, at the Hebrew 
+% University of Jerusalem.
 %
-% This MATLAB file contains code for simulating isoclinic patterns and animating 
-% isoclinic changes on a photoelastic disc, according to two theoretical models 
-% of stress maps - the Ovals model and to the Wulff net model, as explained in 
-% our paper "photoelasticity_report" (sections 1,2,4) and demonstrated 
-% in the "video_appendix" presenetation (both files are attached).
-%
-% Code was written as part of a photoelsticity experiment, carried out in LAB B
-% course of our undergarduate physics studies, at the Hebrew University of Jerusalem.
-% 
 % Created by Shlomo Danziger and Guy Elfersy (c) 
 % 
 % Comments, questions, etc. are welcome:
 % ShlomoMD@gmail.com
-%
+%%
 
 %% ADJUSTABLE GENERAL SETTINGS
 
